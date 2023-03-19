62e334641da2be18bfdeacc6c10f8eeb4e46e0aa
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-03-19 16:34:22 -0300

    adicionando o .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -18,13 +18,13 @@ import { addDoc, collection, updateDoc, doc, getDocs, getFirestore, query, where
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBEsNnelqRpDcVtCjX1hD9thwp2XFjXKas",
-  authDomain: "resumofacul-17214.firebaseapp.com",
-  projectId: "resumofacul-17214",
-  storageBucket: "resumofacul-17214.appspot.com",
-  messagingSenderId: "686964939914",
-  appId: "1:686964939914:web:e88edd40e92cc63afb6173",
-  measurementId: "G-RN3T4J8M4S",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
