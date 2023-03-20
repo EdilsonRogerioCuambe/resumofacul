f143f4fa59744f6642b6d68bd5395b117c2ac4c1
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-03-19 22:12:58 -0300

    melhorando ainda mais

--- src/pages/EditarMateria.jsx
+++ src/pages/EditarMateria.jsx
@@ -20,6 +20,39 @@ const EditarMateria = () => {
 
   const navigate = useNavigate();
 
+  const modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ 'header': 1 }, { 'header': 2 }], // custom button values
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      [{ 'script': 'sub' }, { 'script': 'super' }], // superscript/subscript
+      [{ 'indent': '-1' }, { 'indent': '+1' }], // outdent/indent
+      [{ 'direction': 'rtl' }], // text direction
+
+      [{ 'size': ['small', false, 'large', 'huge'] }], // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ 'color': [] }, { 'background': [] }], // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+
+      ['clean'], // remove formatting button
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    }
+  }
+
+  const formats = [
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'video', 'color'
+  ]
+
   const editarMateria = async (e) => {
     e.preventDefault();
 
@@ -158,7 +191,9 @@ const EditarMateria = () => {
                           Descrição
                         </label>
                         <ReactQuill
-                          className=""
+                          theme="snow"
+                          modules={modules}
+                          formats={formats}
                           value={materia.descricao}
                           onChange={(value) => setMateria({ ...materia, descricao: value })}
                         ></ReactQuill>
